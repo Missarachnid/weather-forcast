7dfb7a7c7e0820be4bc33af729ae470f8ab00555
M. Kepler <mmkepler@yahoo.com>
2017-02-11 12:03:21 -0500

    testing geolookup

--- controllers/controller1.js
+++ controllers/controller1.js
@@ -10,7 +10,7 @@ angular.module('myapp')
       var lati = position.coords.latitude;
       var longi = position.coords.longitude;
       /*var url = 'https://api.wunderground.com/api/4a82154c39d11213/conditions/geolocation/q/' + lati + ',' + longi + '.json';*/
-      var url = 'https://api.wunderground.com/api/4a82154c39d11213/forecast/geolocation/q/' + lati + ',' + longi + '.json';
+      var url = 'https://api.wunderground.com/api/4a82154c39d11213/geolookup/q/' + lati + ',' + longi + '.json';
       $http.get(url)
         .then(function(info) {
         console.log(info);
