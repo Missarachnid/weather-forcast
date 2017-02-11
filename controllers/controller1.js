9a55c59948f637e55d77c4564cf84decb212eff1
M. Kepler <mmkepler@yahoo.com>
2017-02-11 14:49:24 -0500

    Update controller1.js

--- controllers/controller1.js
+++ controllers/controller1.js
@@ -14,7 +14,7 @@ angular.module('myapp')
       //var longi = position.coords.longitude;
       /*var url = 'https://api.wunderground.com/api/4a82154c39d11213/conditions/geolocation/q/' + lati + ',' + longi + '.json';*/
      // var url = 'https://api.wunderground.com/api/4a82154c39d11213/geolookup/q/' + lati + ',' + longi + '.json';
-      var url = "https://api.wunderground.com/api/4a82154c39d11213/forecast/q/autoip.json";
+      var url = "https://api.wunderground.com/api/4a82154c39d11213/geolookup/conditions/forecast/q/autoip.json";
       $http.get(url)
         .then(function(info) {
           console.log(info);
@@ -24,6 +24,7 @@ angular.module('myapp')
           $scope.retrieveCity($scope.location.country, $scope.location.state, $scope.location.city)
       
         });
+        startForecast();
     //}
     /*$scope.retrieveCity = function(country, state, city){
      city = city.replace(" ", "_");
