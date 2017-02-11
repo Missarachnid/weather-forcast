8acb111e59e58633f00dbbe459728cf32d419238
M. Kepler <mmkepler@yahoo.com>
2017-02-11 12:24:19 -0500

    Update controller1.js

--- controllers/controller1.js
+++ controllers/controller1.js
@@ -3,7 +3,9 @@
 
 angular.module('myapp')
   .controller("WeatherController", function($scope, $http){
-  
+    $scope.location = {};
+    $scope.weather = {};
+    $scope.cityForecast = "";
     if (navigator.geolocation) navigator.geolocation.getCurrentPosition(onPositionUpdate);
  
     function onPositionUpdate(position) {
@@ -13,14 +15,29 @@ angular.module('myapp')
       var url = 'https://api.wunderground.com/api/4a82154c39d11213/geolookup/q/' + lati + ',' + longi + '.json';
       $http.get(url)
         .then(function(info) {
-        console.log(info);
-            
-          $scope.weatherNow = {
-            
-          };
-        
+          console.log(info);
+          $scope.location.city = info.data.location.city;
+          $scope.location.state = info.data.nearby_weather_stations.state;
+          $scope.location.country = info.data.country;
+          $scope.retrieveCity($scope.location.country, $scope.location.state, $scope.location.city)
       
-     });
+        });
     }
+    $scope.retrieveCity = function(country, state, city){
+     city = city.replace(" ", "_");
+      
+     if(country === "US"){
+       $scope.cityForecast = 'https://api.wunderground.com/api/4a82154c39d11213/forecast/q/' + state + "/" + city + ".json";
+     }else{
+        $scope.cityForecast = 'https://api.wunderground.com/api/4a82154c39d11213/forecast/q/' + country + "/" + city + ".json
+     }
+     $http.get($scope.cityForecast)
+      .then(function(data){
+       console.log(data);
+     });
+     
+      
+   };
+  
   });
   
