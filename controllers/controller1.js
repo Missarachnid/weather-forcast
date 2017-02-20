3b1f973a59ebdbbc9210e86d5b09f3c1617fad0a
M. Kepler <mmkepler@yahoo.com>
2017-02-20 06:41:27 -0500

    Update controller1.js

--- controllers/controller1.js
+++ controllers/controller1.js
@@ -6,25 +6,25 @@ angular.module('myapp')
     $scope.location = {};
     $scope.weather = {};
     $scope.cityForecast = "";
-    //if (navigator.geolocation) navigator.geolocation.getCurrentPosition(onPositionUpdate);
+    if (navigator.geolocation) navigator.geolocation.getCurrentPosition(onPositionUpdate);
      
-      function startForecast(){
-    //function onPositionUpdate(position) {
-      //var lati = position.coords.latitude;
-      //var longi = position.coords.longitude;
-      /*var url = 'https://api.wunderground.com/api/4a82154c39d11213/conditions/geolocation/q/' + lati + ',' + longi + '.json';*/
-     // var url = 'https://api.wunderground.com/api/4a82154c39d11213/geolookup/q/' + lati + ',' + longi + '.json';
-      var url = "https://api.wunderground.com/api/4a82154c39d11213/forecast/q/autoip.json";
+     
+    function onPositionUpdate(position) {
+      var lati = position.coords.latitude;
+      var longi = position.coords.longitude;
+      var url = 'https://api.wunderground.com/api/4a82154c39d11213/conditions/geolocation/q/' + lati + ',' + longi + '.json';
+     
+      
       $http.get(url)
         .then(function(info) {
           console.log(info);
-          $scope.location.city = info.data.location.city;
-          $scope.location.state = info.data.location.nearby_weather_stations.state;
-          $scope.location.country = info.data.country;
-          $scope.retrieveCity($scope.location.country, $scope.location.state, $scope.location.city)
+          //$scope.location.city = info.data.location.city;
+          //$scope.location.state = info.data.location.nearby_weather_stations.state;
+          //$scope.location.country = info.data.country;
+          //$scope.retrieveCity($scope.location.country, $scope.location.state, $scope.location.city)
       
         });
-        startForecast();
+      
     //}
     /*$scope.retrieveCity = function(country, state, city){
      city = city.replace(" ", "_");
