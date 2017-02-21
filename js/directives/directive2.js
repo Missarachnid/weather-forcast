0ec3a087fdad137d15e0e322886e1f29770d8b51
M. Kepler <mmkepler@yahoo.com>
2017-02-21 12:48:35 -0500

    Update directive2.js

--- js/directives/directive2.js
+++ js/directives/directive2.js
@@ -1,1 +1,12 @@
 
+//custom directive to insert card html from template for Night forecast
+angular.module("myapp")
+  .directive("nightcardinfo", function(){
+    return { 
+    restrict: 'E', 
+    scope: { 
+      info: '=' 
+    }
+    templateUrl: 'js/directives2/cardNight.html' 
+  }; 
+  });
