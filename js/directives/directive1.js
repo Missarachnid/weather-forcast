37f673a59303a6b2cf53cb1d07a0c605b029a99d
M. Kepler <mmkepler@yahoo.com>
2017-02-21 10:14:26 -0500

    Update directive1.js

--- js/directives/directive1.js
+++ js/directives/directive1.js
@@ -1,1 +1,11 @@
-
+//custom directive to insert card html from template
+angular.module("myapp")
+  .directive("cardinfo", function(){
+    return { 
+    restrict: 'E', 
+     scope: { 
+      
+    }, 
+    templateUrl: 'js/directives/card.html' 
+  }; 
+  });
